7667a0cb8efc1cef9d49463907259f4a8d590bd9
V-Mokhun <mohunmax1408@gmail.com>
2022-11-16 12:52:20 +0100

    Changed refresh token flow

--- src/extensions/users-permissions/strapi-server.js
+++ src/extensions/users-permissions/strapi-server.js
@@ -1,36 +1,36 @@
 // ref doc: https://docs.strapi.io/developer-docs/latest/development/plugins-extension.html#extending-a-plugin-s-interface
 module.exports = (plugin) => {
-  plugin.controllers.auth.refreshToken = async (ctx) => {
-    // refresh userself token
-    // const newJwt = strapi.plugins['users-permissions'].services.jwt.issue({
-    //   id: ctx.state.user.id
-    // })
-    // return { jwt: newJwt }
+  // plugin.controllers.auth.refreshToken = async (ctx) => {
+  // refresh userself token
+  // const newJwt = strapi.plugins['users-permissions'].services.jwt.issue({
+  //   id: ctx.state.user.id
+  // })
+  // return { jwt: newJwt }
 
-    // comment out next lines, and refresh the request body's token like {token: 'xxx'}
-    const { token } = ctx.request.body;
-    const payload =
-      strapi.plugins["users-permissions"].services.jwt.verify(token);
-    const newJwt = strapi.plugins["users-permissions"].services.jwt.issue({
-      id: payload.id,
-    });
-    return { token: newJwt };
+  // comment out next lines, and refresh the request body's token like {token: 'xxx'}
+  // const { token } = ctx.request.body;
+  // console.log("STRAPI TOKEN: ", token);
+  // const payload =
+  //   strapi.plugins["users-permissions"].services.jwt.verify(token);
+  // const newJwt = strapi.plugins["users-permissions"].services.jwt.issue({
+  //   id: payload.id,
+  // });
+  // return { token: newJwt };
+  // };
+
+  plugin.controllers.auth.refreshToken = async (ctx) => {
+    const payload = await strapi.plugins["users-permissions"].services.jwt
+      .verify(ctx.request.body.token)
+      .catch((error) => {
+        return {
+          jwt: strapi.plugins["users-permissions"].services.jwt.issue({
+            id: ctx.request.body.id,
+          }),
+        };
+      });
+
+    return { token: payload.jwt };
   };
-
-  /*
-  plugin.controllers.auth.refreshToken = async (ctx) => {
-		const payload = await strapi.plugins['users-permissions'].services.jwt.verify(ctx.request.body.token)
-		.catch(error => {
-			console.log(error.message)
-			return {jwt: strapi.plugins['users-permissions'].services.jwt.issue({
-				id: ctx.request.body.id
-			})}
-		});
-
-		console.log(payload)
-		return {jwt: payload.jwt}
-	}
-  */
 
   plugin.routes["content-api"].routes.push({
     method: "POST",
